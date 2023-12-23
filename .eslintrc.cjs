ca65b0f7808b2b22fb3987e966ed448af16a6c38
Manuel Lehé <manuellehe@googlemail.com>
2023-12-23 17:26:58 +0100

    infrastructure: prettier & eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,18 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+        'eslint:recommended',
+        "plugin:react/recommended",
+        'plugin:@typescript-eslint/recommended-type-checked',
+        "plugin:prettier/recommended"
+    ],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: __dirname,
+    },
+    rules: {
+        "prettier/prettier": "warn",
+    },
     root: true,
 };
